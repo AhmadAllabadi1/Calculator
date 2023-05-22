e8853efa00d6b76187b243aa9273b6b66bd93ee4
AhmadAllabadi1 <ahmad@Xubuntu.myguest.virtualbox.org>
2023-05-22 11:36:15 +0300

    Commiting

--- script.js
+++ script.js
@@ -2,79 +2,142 @@ const displayT = document.querySelector(".top");
 const displayB = document.querySelector(".bottom");
 const numbers = document.querySelectorAll(".number");
 const operators=document.querySelectorAll(".operator");
+const clear = document.querySelector(".clear");
+const equal=document.querySelector(".equal");
+const del=document.querySelector(".delete");
 
-let num1 = ''
-let num2 = ''
+let firstNum = ''
+let secondNum = ''
 let operator = ''
-let input = ''
-let result = '0'
-
-
+let current=true
+let equalBool = false
 
 window.addEventListener('click', function() {
-    input = num1 + ' ' + operator + ' ' + num2;
-    displayT.textContent = input;
-    displayB.textContent = result;
+    updateTop();
+    updateBottom();
+    equalBool = false;
+    console.log(firstNum);
 })
 
-numbers.forEach(occurence => {
-    occurence.addEventListener('click', function() {
-        let id = occurence.getAttribute('id');
-        appendNumber(id);
-    })
-})
-
-
-
-operators.forEach(occurence => {
-    occurence.addEventListener('click', function() {
-        let id = occurence.getAttribute('id');
-        updateOperator(id);
-    })
-})
-
-
-
-
-function updateOperator(operation) {
-    if (num1!='' && operator!='' && num2!='') {
-        result = operate();
-        num1 = result;
-        num2 = ''
+del.addEventListener('click', function() {
+    if (firstNum!=''&&secondNum===''&&operator===''){
+        firstNum=firstNum.substring(0,firstNum.length-1);
+    }
+    else if (firstNum!=''&&operator!=''&&secondNum==='') {
         operator = ''
     }
 
+    else{
+        secondNum = secondNum.substring(0,secondNum.length-1);
+    }
+})
 
-    if (num1!=''){
-        if (num2==='') {
-            operator = operation;
+equal.addEventListener('click',function () {
+    if (secondNum===''){}
+    else{equalBool=true}
+})
+
+
+operators.forEach(occurence => {
+    occurence.addEventListener('click', function () {
+        operatorClick(occurence.getAttribute("id"));
+    })
+})
+
+numbers.forEach(occurence => {
+    occurence.addEventListener('click',function(){
+        appendNumber(occurence.getAttribute('id'));
+    })
+})
+
+
+clear.addEventListener('click', function() {
+    firstNum = ''
+    secondNum = ''
+    operator = ''
+    current=true
+    updateBottom();
+    updateTop();
+})
+
+
+function updateTop(){
+
+    if (equalBool===true) {
+        if (operator==="x"){
+            displayT.textContent = firstNum + ' ' + String.fromCharCode(215) + ' ' + secondNum + ' =' ;    
         }
-
+        else if (operator==='/') {
+            displayT.textContent = firstNum + ' ' + String.fromCharCode(247) + ' ' + secondNum + ' =';    
+     
+        } 
         else{
-            input = num1 + ' ' + operator+' ' + num2
-        }
+            displayT.textContent = firstNum + ' ' + operator + ' ' + secondNum+' =';    
+            }   
+    }
+
+    else{
+    if (operator==="x"){
+        displayT.textContent = firstNum + ' ' + String.fromCharCode(215) + ' ' + secondNum;    
+    }
+    else if (operator==='/') {
+        displayT.textContent = firstNum + ' ' + String.fromCharCode(247) + ' ' + secondNum;    
+ 
+    }
+    else{
+    displayT.textContent = firstNum + ' ' + operator + ' ' + secondNum;    
+    }
+    }}
+
+
+function updateBottom(){
+    if(equalBool===true){
+        displayB.textContent = operate();
+    }
+    else{
+    if (current===true){
+        displayB.textContent = firstNum;
+    }
+    else{
+        displayB.textContent = secondNum;
+    }
+    }}
+
+
+function operatorClick(operation) {
+
+    if (firstNum!=''&&secondNum==='') {
+        operator=operation;
+        current=false;
+    }
+
+    if (firstNum!=''&&secondNum!=''&&operator!='') {
+        firstNum = operate();
+        secondNum = ''
+        operator = operation;
+        current=false;
     }
 }
-
 
 function appendNumber(num) {
     if (operator === '') {
-        if (num1 === '') {
-            num1 = num
+        if (firstNum === '') {
+            firstNum = num
         }
         else{
-            num1+=num
+            firstNum+=num
         }
     }
     else{
-        if (num2 === '') {
-            num2 = num
+        if (secondNum === '') {
+            secondNum = num
         }
         else{
-            num2+=num
+            secondNum+=num
         }
     }
 }
+
 
 function add(a,b) {
     return (a+b).toString();
@@ -94,16 +157,16 @@ function divide(a,b) {
 
 function operate() {
     if (operator === "+") {
-        return add(parseInt(num1),parseInt(num2));
+        return add(parseInt(firstNum),parseInt(secondNum));
     }
     else if (operator === "-") {
-        return subtract(parseInt(num1),parseInt(num2));
+        return subtract(parseInt(firstNum),parseInt(secondNum));
     }
     else if (operator === "x") {
-        return multiply(parseInt(num1),parseInt(num2));
+        return multiply(parseInt(firstNum),parseInt(secondNum));
     }
     else if (operator === "/") {
-        return divide(parseInt(num1),parseInt(num2));
+        return divide(parseInt(firstNum),parseInt(secondNum));
     }
 
 }
